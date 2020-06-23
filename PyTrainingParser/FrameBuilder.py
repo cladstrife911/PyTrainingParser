2dc968e814a1991f4e464d3a3dc53825568f580f
Antoine Monmarché <antoine.monmarche@valeo.com>
2020-06-23 17:35:34 +0200

    fix FrameBuilder to parse log with several anchors
    Change-Id: Ifdbd383cf0bc3f8a4f2b996af712bf053af478f7

--- PyTrainingParser/FrameBuilder.py
+++ PyTrainingParser/FrameBuilder.py
@@ -41,24 +41,34 @@ class FrameBuilder:
                     self._version = "U3"
                     #print("Hub Time:",row[2])
                     #print("Ranging counter:",row[3])
-                    #print("json:",row[4])
+                    #print("U3 json:",row[4])
+                    line = row[4].replace('\'','"')
+                    #print("U3 line:",line)
                     try:
-                        parsedData = json.loads(row[4])
-                        if self._anchorId:
+                        parsedData = json.loads(line)
+                        #print("_anchorId",self._anchorId)
+                        if self._anchorId in parsedData:
                             self._hubTimeArray = np.append(self._hubTimeArray, int(row[2]))
                             self._counterArray = np.append(self._counterArray, int(row[3]))
-                            #print("Distance:",parsedData[gArgs.anchor]["d"])
+                            #print("self._hubTimeArray ",self._hubTimeArray)
+                            #print("self._counterArray ",self._counterArray)
+                            #print("Distance:",parsedData[self._anchorId]["d"])
                             self._distanceArray = np.append(self._distanceArray, float(parsedData[self._anchorId]["d"]))
                             self._fpidxArray = np.append(self._fpidxArray, float(parsedData[self._anchorId]["dt"]))
                             self._overallrxpArray = np.append(self._overallrxpArray, float(parsedData[self._anchorId]["snr"]))
                             self._fppArray = np.append(self._fppArray, float(parsedData[self._anchorId]["dPow"]))
-                    except json.decoder.JSONDecodeError:
-                        print("Error on incomplete line")
+                    except json.decoder.JSONDecodeError as error:
+                        print("Error on incomplete line:", error)
+                    except IndexError:
+                        print("Error on incomplete line (IndexError)")
+                    except KeyError:
+                        print("Key ", self._anchorId,"not found")
+
                 elif (row[1]=="U3.1"):
                     self._version = "U3.1"
                     try:
                         parsedData = json.loads(row[4])
-                        if self._anchorId:
+                        if self._anchorId in parsedData:
                             self._hubTimeArray = np.append(self._hubTimeArray, int(row[2]))
                             self._counterArray = np.append(self._counterArray, int(row[3]))
                             self._distanceArray = np.append(self._distanceArray, float(parsedData[self._anchorId]["d"]))
@@ -67,11 +77,14 @@ class FrameBuilder:
                             self._fppArray = np.append(self._fppArray, float(parsedData[self._anchorId]["pF"]))
                     except json.decoder.JSONDecodeError:
                         print("Error on incomplete line")
+                    except IndexError:
+                        print("Error on incomplete line (IndexError)")
                 elif (row[1]=="U3.2"):
                     self._version = "U3.2"
                     try:
+                        print("Hub Time:",row[2])
                         parsedData = json.loads(row[4])
-                        if self._anchorId:
+                        if self._anchorId in parsedData:
                             self._hubTimeArray = np.append(self._hubTimeArray, int(row[2]))
                             self._counterArray = np.append(self._counterArray, int(row[3]))
                             self._distanceArray = np.append(self._distanceArray, float(parsedData[self._anchorId]["d"]))
@@ -81,6 +94,11 @@ class FrameBuilder:
                             self._dIArray = np.append(self._dIArray, float(parsedData[self._anchorId]["dI"]))
                     except json.decoder.JSONDecodeError:
                         print("Error on incomplete line")
+                    except IndexError:
+                        print("Error on incomplete line (IndexError)")
+                else:
+                    print("line skip")
+
 
             #print(self._hubTimeArray.size, self._counterArray.size,self._distanceArray.size,self._fpidxArray.size,self._overallrxpArray.size,self._fppArray.size)
             if self._version == "U3.2":
